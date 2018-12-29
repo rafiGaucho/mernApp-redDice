df7e9075af9ef70cf928059618bb0b1af80b4ddd
rafiGaucho <rafigaucho10r@gmail.com>
2018-12-29 07:47:40 +0530

    hot reload

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
-let app = express();
-
 import path from 'path';
+
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
-import webpackConfig from '../webpack.config.dev.js'
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
 
-app.get('/*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'./index.html'))
-})
+let app = express();
 
-app.listen(3000 , ()=>console.log('running on localhost:3000'))
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(3000, () => console.log('Running on localhost:3000'));
